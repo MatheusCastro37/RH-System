992d0e312ffc4ca2c78ebd82bd29a31a72f7d0ef
henrique-olivier <henrique.rosa@sptech.school>
2024-11-08 09:05:55 -0300

    dashboard finalizada

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import { useCallback, useEffect, useState } from "react";
 import Typography from "../../Typography";
 import { BodyContainer, CardContainer, Select } from "./styles";
-import { PieChart, Pie, Sector } from 'recharts';
+import { PieChart, Pie, Sector, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Rectangle } from 'recharts';
 import { carrers, collaborator, payload, propsGraph, selectType } from "./types";
 import { theme } from "../../colors/colorts";
 
 let textTooltip: string;
+
+interface Collaborator {
+  id?: number;
+  nome: string;
+  cpf: string;
+  cep: string;
+  logradouro: string;
+  numero: string;
+  cidade: string;
+  estado: string;
+  idCargo: number;
+}
+interface PositionType {
+  id: number;
+  nomeDoCargo: string;
+  nivel: string;
+  salario: number;
+}
+
+interface ChartData {
+  cargo: string;
+  Quantidade: number;
+  salario: number;
+}
 
 const renderActiveShape = ({payload, ...props}: propsGraph) => {
   const RADIAN = Math.PI / 180;
@@ -201,13 +225,138 @@ export default function Dashboard() {
 
   }
 
+  const [collaboratorList, setCollaboratorList] = useState<Collaborator[]>()
+  const [cargos, setCargos] = useState<PositionType[]>();
+
+  useEffect(() => { getCollaborators() }, [])
+  useEffect(() => { getPositions() }, [])
+
+  async function getCollaborators() {
+      try {
+          const res = await fetch(`${supabaseUrl}/rest/v1/Colaborador`, {
+              headers: {
+                  method: 'GET',
+                  apikey
+              }
+          })
+
+          if (!res) {
+              throw new Error
+          }
+
+          const data: Collaborator[] = await res.json();
+          setCollaboratorList(data)
+          console.log(data)
+      } catch (error) {
+          console.error("Erro ao buscar Cargos na API: " + error)
+
+      }
+  }
+
+  async function getPositions(): Promise<PositionType[] | null> {
+      try {
+
+          const res = await fetch(`${supabaseUrl}/rest/v1/Cargo`, {
+              headers: {
+                  method: 'GET',
+                  apikey
+              }
+          })
+
+          if (!res) {
+              throw new Error
+          }
+
+          const data: PositionType[] = await res.json();
+          setCargos(data)
+
+          return data
+      } catch (error) {
+          console.error("Erro ao buscar Cargos na API: " + error)
+          return null;
+      }
+  }
+
+  function setDataForChart() {
+      const data: ChartData[] = [
+
+      ]
+
+
+
+      if (collaboratorList && cargos) {
+
+
+          collaboratorList.map(collaborator => {
+              let carrerExist = false
+
+              const collabCarrer = cargos.find(cargo => cargo.id == collaborator.idCargo)
+
+              for (const i of data) {
+                  if (i.cargo == collabCarrer?.nomeDoCargo) {
+                      carrerExist = true;
+                      i.Quantidade++;
+                  }
+              }
+
+              if (!carrerExist && collabCarrer) {
+                  data.push({
+                      cargo: collabCarrer.nomeDoCargo,
+                      Quantidade: 1,
+                      salario: collabCarrer.salario
+                  })
+              }
+          })
+
+
+      }
+      console.log(data);
+      return data;
+  }
+
+
+  interface CustomTooltipProps {
+      active?: boolean;
+      payload?: {
+          payload: {
+              cargo: string;
+              Quantidade: number;
+              salario: number;
+          };
+      }[];
+      label?: string;
+  }
+
+  const CustomTooltip = ({ active, payload, label }: CustomTooltipProps) => {
+      if (active && payload && payload.length) {
+          const { cargo, Quantidade, salario } = payload[0].payload;
+          return (
+              <div style={{ backgroundColor: '#fff', border: '1px solid #ccc', padding: '10px' }}>
+                  <p><strong>{cargo}</strong></p>
+                  <p>Quantidade: {Quantidade}</p>
+                  <p>Salário: R$ {salario.toFixed(2)}</p>
+              </div>
+          );
+      }
+
+      return null;
+  };
+
   return(
       <BodyContainer>
           <CardContainer>
-              <Typography variant="H4">Olá, bem vindo fulano de tal</Typography>
+              <Typography variant="H4">Bem vindo</Typography>
           </CardContainer>
           <CardContainer>
-              <Typography variant="H4">Grafico 1</Typography>
+          <Typography variant="H4">Distribuição de Colaboradores por Cargo</Typography>
+                <BarChart width={1000} height={250} data={setDataForChart()} margin={{ top: 5, right: 20, left: 20, bottom: 5, }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="cargo" />
+                    <YAxis />
+                    <Tooltip content={<CustomTooltip />} />
+                    <Legend />
+                    <Bar dataKey="Quantidade" fill={theme.corporate.purple} activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                </BarChart>
           </CardContainer>
           <CardContainer>
             <div className="graph-container">
